8a68ba701fa90a85effa54d766b0a1d0c5205f0f
Daniel Tavares de Lima Marcelino <toys.tokio@gmail.com>
2021-03-07 21:25:16 -0300

    Modifiquei o código

--- PerCapita/codigo.py
+++ PerCapita/codigo.py
@@ -1,5 +1,4 @@
-from typing import Container
-
+#Perguntas
 
 pergunta = int(input("Quantas pessoas moram com você? "))
 pergunta2 = int(input("Dessas {} pessoas, quantas pessoas ganham salário?".format(pergunta)))
@@ -13,7 +12,7 @@ conta2 = float(conta / salario_minimo)
 #conclusão
 
 if conta2 <= 1.50:
-     print("Sua renda Per Capita é de {} salários minimos\n Como (atualmente) o Fies, Prouni e o Sisu estipulam como base um salário minimo e meio (1,5)\n Você tem direito a 100 percento do beneficio".format(conta2))
+     print("Sua renda Per Capita é de {:.2f} salários minimos.\n Como (atualmente) o Fies, Prouni e o Sisu estipulam como base um salário minimo e meio (1,5)\n Você tem direito a 100 percento do beneficio".format(conta2))
 
 else:
-     print("Sua renda Per Capita é de {} salários minimos\n Como (atualmente) o Fies, Prouni e o Sisu estipulam como base um salário minimo e meio (1,5)\n Você tem não direito a 100 percento do beneficio".format(conta2))
+     print("Sua renda Per Capita é de {:.2f} salários minimos\n Como (atualmente) o Fies, Prouni e o Sisu estipulam como base um salário minimo e meio (1,5)\n Você tem direito a 50 percento do beneficio".format(conta2))
